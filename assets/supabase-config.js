3758c6f1b1bfab70ff8fbd33afcaa59a94422ebe
ingridrss <ingridraquel2@gmail.com>
2026-08-20 17:09:13 -0300

    Update supabase-config.js

--- assets/supabase-config.js
+++ assets/supabase-config.js
@@ -1,4 +1,7 @@
 // Preencha com os dados do seu projeto Supabase.
 // Project Settings > API
-window.LEGISTA_SUPABASE_URL = "COLE_AQUI_SUA_PROJECT_URL";
-window.LEGISTA_SUPABASE_ANON_KEY = "COLE_AQUI_SUA_ANON_PUBLIC_KEY";
+window.LEGISTA_SUPABASE_URL =
+  "https://rwdrqipzdfkmrjeceddd.supabase.co";
+
+window.LEGISTA_SUPABASE_ANON_KEY =
+  "sb_publishable_q3Q8TmXtBMoV1S9c59l7ew_Z_HTFyla";
